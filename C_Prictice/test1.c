b95d24aead8841c94ffabfab90da03f20bae6f31
runny <842655705@qq.com>
2020-05-22 11:54:27 +0800

    delete variable k;

--- C_Prictice/test1.c
+++ C_Prictice/test1.c
@@ -11,7 +11,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     int i;
     int j;
-    int k;
     printf("请输入两个数字!");
     scanf("%d %d",&i,&j);
     printf("Hello, World! %d\n",i+j);
