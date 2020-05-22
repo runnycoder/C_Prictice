04002fe0c41d9a3200398a396e920ab6cf8574fe
runny <842655705@qq.com>
2020-05-22 11:13:26 +0800

    c first test

---
+++ C_Prictice/test1.c
@@ -0,0 +1,18 @@
+//
+//  test1.c
+//  C_Prictice
+//
+//  Created by runny on 2020/5/22.
+//  Copyright © 2020 runny. All rights reserved.
+//
+
+#include <stdio.h>
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int i;
+    int j;
+    printf("请输入两个数字!");
+    scanf("%d %d",&i,&j);
+    printf("Hello, World! %d\n",i+j);
+    return 0;
+}
