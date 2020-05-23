c9584a9f4970116e9eba7a9ac08ea8b695f37ecf
runny <842655705@qq.com>
2020-05-23 22:49:34 +0800

    新增循环控制练习题

--- C_week1/main.c
+++ C_week1/main.c
@@ -16,8 +16,20 @@ int main(int argc, const char * argv[]) {
     
 //    void  loopTest();
 //    loopTest();
-    void reverseIntNum();
-    reverseIntNum();
+//    void reverseIntNum();
+//    reverseIntNum();
+    
+//    void isPrimeNumber();
+//    isPrimeNumber();
+    
+//    void printfFiftyPrimeNuber();
+//    printfFiftyPrimeNuber();
+    
+//    void splitPrintReadNumber();
+//    splitPrintReadNumber();
+    
+    void splitPrintReadNumber2();
+    splitPrintReadNumber2();
     return 0;
 }
 
@@ -71,5 +83,105 @@ void reverseIntNum(){
     
     printf("逆序完成的整数为%d\n",reverseNum);
        
+    
+}
+
+//判断一个数是否为素数
+void isPrimeNumber(){
+    int readNumber;
+    printf("请输入需要判断的数值!");
+    scanf("%d",&readNumber);
+    int isPrimeFlag = 1;
+    int i ;
+    for (i=2; i<readNumber; i++) {
+        if(readNumber%i==0){
+            isPrimeFlag = 0;
+            break;
+        }
+    }
+    if(isPrimeFlag==1&&readNumber!=1){
+        printf("输入值%d是素数",readNumber);
+    }else{
+        printf("输入值%d不是素数",readNumber);
+    }
+}
+
+//输出前五十个素数
+void printfFiftyPrimeNuber(){
+    int readNumber=2;
+    int loopCount = 0;
+    
+    while (loopCount<50) {
+        int isPrimeFlag = 1;
+        int i ;
+        for (i=2; i<readNumber; i++) {
+            if(readNumber%i==0){
+                isPrimeFlag = 0;
+                break;
+            }
+        }
+        if(isPrimeFlag==1&&readNumber!=1){
+            printf("%d\t",readNumber);
+            loopCount++;
+            if(loopCount%5==0){
+                printf("\n");
+            }
+        }
+       
+        readNumber++;
+    }
+    
+}
+
+//12345-> 1 2 3 4 5 但是这种对于含0的整数就会出现异常
+void splitPrintReadNumber(){
+    int readNum ;
+    int reverseNum=0 ;
+    int digit;
+    printf("请输入合适的正整数!\n");
+    scanf("%d",&readNum);
+    while (readNum>0) {
+         digit = readNum%10;
+         reverseNum = reverseNum*10+digit;
+         readNum/=10;
+    }
+    printf("reverseNum=%d\n",reverseNum);
+    
+    while(reverseNum>0){
+        digit = reverseNum%10;
+        printf("%d",digit);
+        if(reverseNum>=10){
+            printf(" ");
+        }
+        reverseNum/=10;
+    }
+    printf("\n");
+   
+}
+//12345-> 1 2 3 4 5
+void splitPrintReadNumber2(){
+    int readNum ;
+    int splitNum=0 ;
+    int preDigit;
+    int maxUnit = 1;
+    int divideCount = 0;
+    printf("请输入合适的正整数!\n");
+    scanf("%d",&readNum);
+    splitNum=readNum;
+    
+    //循环求取最高位数值
+    while (readNum>=10) {
+        readNum/=10;
+        divideCount++;
+        maxUnit*=10;
+    }
+    
+    //从高到低输出每一位数值
+    while(maxUnit>0){
+        preDigit=splitNum/maxUnit;
+        splitNum%=maxUnit;
+        maxUnit/=10;
+        printf("%d\n",preDigit);
+    }
     
 }
